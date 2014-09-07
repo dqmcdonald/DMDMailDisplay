57707a1ab0fe8cd36f84a37ae394d05da2aa4bfa
Quentin McDonald <dqmcdonald@gmail.com>
2014-09-08 11:40:48 +1200

    Added sending via RFID card
    RFID card messages come from second Arduino via serial on pin 15
    
    Added comments and tidied up code a bit.

--- DMDMailDisplay.ino
+++ DMDMailDisplay.ino
@@ -1,41 +1,104 @@
 
+/*
+DMDMailDisplay
+
+
+Fetch Mail from a Gmail account and display it on a FreeTronics DMD LED Matrix display. Mail is actually fetched via
+a Python script.
+
+
+Uses Dragino Yun Shield (http://www.dragino.com/products/yunshield/item/86-yun-shield.html) and Freetronics DMD
+LED Matrix (http://www.freetronics.com/products/dot-matrix-display-32x16-red#.VAzpwGS1YY4).
+
+
+Also listen for a serial connection on PIN 15. This should be a byte that represents a message ID
+
+
+Maximum length of email message is 128 characters.
+
+
+Quentin McDonald
+September 2014
+
+*/
+
 
 #include <SPI.h>
 #include <DMD2.h>
 #include <fonts/SystemFont5x7.h>
 #include <fonts/Arial14.h>
 #include <Process.h>
-#include<FileIO.h>
 #include<Bridge.h>
+#include <Console.h>
+#include <SoftwareSerial.h>
 
 const uint8_t *FONT =  Arial14;
-SoftDMD dmd(1, 1); // DMD controls the entire display
+SPIDMD dmd(1, 1); // DMD controls the entire display
 
+// This script will be copied to the YunShield when uploaded
 #define SCRIPT "/mnt/sda1/arduino/www/DMDMailDisplay/DMDMailDisplay.py"
 
+#define CHARACTER_DELAY 150   //  delay after printing each character on the screen
+#define TEXT_DELAY 1000       //  delay after printing each text before the screen is cleared
+
+const char* FETCHING_EMAIL_MESSAGE = "Fetching...";
+const char* DONE_EMAIL_MESSAGE = "Done";
+
+int message_serial_num = 0; // Give each message a unique serial number in the subject line
+
+
+const char* messages[] = {
+  "Hi There",
+  "Thanks",
+  "Love you",
+  "Miss you too"
+};
+
+
+// Uncomment the following line to get debugging messages printed to the Console:
+//#define DEBUG 1
+
+#define RX_PIN 15
+#define TX_PIN 16
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX,TX
+
+// This buffer determines the max. size of the email message.
 char buff[128];
 
 int i = 0;
 char c;
+char c1, c2;
+
+
 unsigned long  check_mail_now = 1;
-const unsigned long CHECK_MAIL_PERIOD = 120000; // Check every two minutes
+const unsigned long CHECK_MAIL_PERIOD = 180000; // Check every three minutes
 
 unsigned long display_message_now = 1;
-const unsigned long DISPLAY_MESSAGE_PERIOD = 10000; // Display every 10 seconds
+const unsigned long DISPLAY_MESSAGE_PERIOD = 20000; // Display every 20 seconds
 
 
 void setup() {
+
+
   Bridge.begin();
-  // put your setup code here, to run once:
+
+
+#ifdef DEBUG
+  Console.begin();
+  while (!Console) {
+    ; // wait for Console port to connect.
+  }
+  Console.println("Beginning");
+#endif
 
   dmd.setBrightness(100);
   dmd.selectFont(FONT);
   dmd.begin();
 
-  FileSystem.begin();
-
   Bridge.put("DMDTODISP", "Nothing");
 
+  mySerial.begin(4800);
+  mySerial.flush();
 }
 
 
@@ -43,33 +106,120 @@ void loop() {
 
   // Check for new mail from time to time:
   if ( millis() > check_mail_now ) {
+#ifdef DEBUG
+    Console.println("Fetching mail");
+#endif
+    displayText(FETCHING_EMAIL_MESSAGE);
     check_mail_now = millis() + CHECK_MAIL_PERIOD;
     Process p;
     p.begin("python");
     p.addParameter(SCRIPT);
+    p.addParameter("FETCH");
     p.run();
+    displayText(DONE_EMAIL_MESSAGE);
+#ifdef DEBUG
+    Console.println("Done fetching mail");
+#endif
   }
 
+
+  // Display the message from time to time:
   if ( millis() > display_message_now ) {
-    DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
 
- 
+
     Bridge.get("DMDTODISP", buff, 128 );
-    for ( i = 0; i < strlen(buff); i++ ) {
-
-      box.print(buff[i]);
-      delay(200);
-    }
-
-
-
-    delay(1000);
-    dmd.clearScreen();
-
-
-
+    displayText( buff);
     display_message_now = millis() + DISPLAY_MESSAGE_PERIOD;
   }
 
+  delay(500);  // Delay is simply to avoid wasting any cycles calling all this too ofen
+
+
+  // Check for Serial bytes on pin 15.
+  if ( mySerial.available() ) {
+    int uid = (int)mySerial.read();
+#ifdef DEBUG
+    Console.print("Found Card: " );
+    Console.println( uid, DEC );
+#endif
+    int msg_index = getMessageIndex(uid);
+#ifdef DEBUG
+    Console.print("Message Index: " );
+    Console.println( msg_index, DEC );
+#endif
+    if ( msg_index >= 0 ) {
+      sendEmail( msg_index );
+
+    }
+  }
 
 }
+
+
+// Display a line, character by character on the LED matrix:
+
+void displayText( const char * txt ) {
+  DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
+
+  dmd.clearScreen();
+  for ( unsigned j = 0; j < strlen(txt); j++ ) {
+    box.print(txt[j]);
+    delay(CHARACTER_DELAY);
+  }
+  delay(TEXT_DELAY);
+  dmd.clearScreen();
+}
+
+
+// Convert a RFID ID into a message index, an index into the array of possible messages:
+int getMessageIndex( int uid ) {
+  // Convert RFID Card UID into message index:
+  int idx = -1;
+
+  switch ( uid ) {
+    case 244:
+      idx = 0;
+      break;
+    case 84:
+      idx = 1;
+      break;
+    case 37:
+      idx = 2;
+      break;
+    case 205:
+      idx = 3;
+      break;
+    default:
+      idx = -1;
+  }
+
+  return idx;
+
+}
+
+
+// Send an email based on the message index 'idx'
+void sendEmail( int idx ) {
+
+#ifdef DEBUG
+  Console.println("Sending mail");
+#endif
+  displayText("Sending...");
+  displayText(messages[idx]);
+  Bridge.put("TOEMAIL", messages[idx]);
+  message_serial_num += 1;
+  sprintf(buff,"%d", message_serial_num );
+  Bridge.put("EMAILSER", buff);
+  Process p;
+  p.begin("python");
+  p.addParameter(SCRIPT);
+  p.addParameter("SEND");
+  p.run();
+#ifdef DEBUG
+  Console.println("Done sending mail");
+#endif
+  displayText("Message sent");
+
+}
+
+
--- www/DMDMailDisplay.py
+++ www/DMDMailDisplay.py
@@ -1,21 +1,32 @@
-
+#
+#  Python script to fetch and send mail.
+#
 #!/usr/bin/env python
 
 import sys
 import imaplib
 import email
-import private as private
+import smtplib
 
+
+# Note this module is not uploaded to github. It needs to define:
+#EMAIL=<gmail user name>
+#PASSWORD=<gmail password>
+#FROM=<the only address messages will be displayed from>
+#TO=<where email messsages should be sent to>
+import private as private  
+
+# We need to find the bridge module:
 sys.path.insert(0, '/usr/lib/python2.7/bridge/') 
                                                 
 from bridgeclient import BridgeClient as bridgeclient
 
 value = bridgeclient()
 
-# Note: This function definition needs to be placed
-#       before the previous block of code that calls it.
+
+# Process the IMAP mail box that's already been opened:
 def process_mailbox(M):
-  rv, data = M.search(None,'All')
+  rv, data = M.search(None,'(FROM "%s")' % private.FROM)  # Restrict messages to those from a particular address
   if rv != 'OK':
       print "No messages found!"
       return
@@ -29,7 +40,7 @@ def process_mailbox(M):
           print "ERROR getting message", num
           return
 
-               
+      # Handle both multi-part and single part messages:
       msg = email.message_from_string(data[0][1])
       if msg.is_multipart():                     
         p = msg.get_payload(0)                   
@@ -40,25 +51,50 @@ def process_mailbox(M):
       for l in lines.split('\n'):                
           if not l.startswith( "Content-Type:") and len(l) > 1:
             print l                                            
-            value.put("DMDTODISP",l)                               
+            value.put("DMDTODISP",l)       # This will make the message available to teh Arduino                        
                                      
                                                                
               
-      
+# Fetch mail from the gmail server     
+def fetch_mail():
+
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
+
+    try:
+        M.login(private.EMAIL,private.PASSWORD)
+    except imaplib.IMAP4.error:
+        print "LOGIN FAILED!!! "
+        # ... exit or deal with failure...
+
+    rv, data = M.select("INBOX")
+    if rv == 'OK':
+        process_mailbox(M) # ... do something with emails, see below ...
+        M.close()
+    M.logout()
 
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+def send_mail():
+    # These next two values will be set by teh Arduino via the Bridge:
+    body=value.get("TOEMAIL")
+    ser=value.get("EMAILSER")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(private.EMAIL, private.PASSWORD)
+    header = 'To:' + private.TO + '\n' + 'From: ' + private.EMAIL
+    header = header + '\n' + 'Subject:' + 'Message from home #%d' % int(ser)  + '\n'
+    msg = header + '\n' + body + ' \n\n' + '\n\n'
+    print msg
+    smtpserver.sendmail(private.EMAIL, private.TO, msg)
+    smtpserver.close()
 
-try:
-    M.login(private.EMAIL,private.PASSWORD)
-except imaplib.IMAP4.error:
-    print "LOGIN FAILED!!! "
-    # ... exit or deal with failure...
 
-rv, data = M.select("INBOX")
-if rv == 'OK':
-    process_mailbox(M) # ... do something with emails, see below ...
-    M.close()
-M.logout()
+    
 
-
+    
+if __name__ == "__main__":
+    if sys.argv[1] == "FETCH":
+        fetch_mail()
+    elif sys.argv[1] == "SEND":
+        send_mail()
